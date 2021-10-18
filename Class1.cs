be97b997564479cf77d181de606e15b31c04fead
vysh1234 <75359385+vysh1234@users.noreply.github.com>
2021-10-18 10:38:02 +0530

    LatestLangFeautures

--- Class1.cs
+++ Class1.cs
@@ -4,51 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demo_Class_Implementation
+namespace Demo_LatestLangFeautures
 {
-    partial class LMS : IAssesment
+    public struct Coordinates
     {
-        string name;
-        int SNo;
-        long MobileNo;
-        string Email;
-        public void Display()
-        {
-            Console.WriteLine("Implementation Void");
-        }
 
-        void IAssesment.MCQ_Assesment()
+        public Coordinates(Double x, Double y)
+            
         {
-            Console.WriteLine("this implementation from LMS");
+            X = x;
+            Y = y;
         }
-
-        void IAssesment.QuestionBank()
-        {
-            Console.WriteLine("retuening value from quetion bank");
-        }
-    }
-    partial class LMS : placement, IAssesment
-    {
-        public void Message()
-        {
-            Console.WriteLine("This message is coming from second imp");
-        }
-    }
-    abstract class placement
-    {
-        string Name_of_company;
-        int No_of_student_placed;
-        DateTime Year_of_placement;
-
-        public void placement_record()
-        {
-            Console.WriteLine("This method will show the record of placement");
-        }
-    }
-    public interface IAssesment
-    {
-        public void MCQ_Assesment();
-        public void QuestionBank();
-
+        public double X { get; }
+        public double Y { get; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,22 +1,40 @@
 ﻿using System;
 
-namespace Demo_Class_Implementation
+namespace Demo_LatestLangFeautures
 {
     class Program
     {
         static void Main(string[] args)
         {
-            LMS Akash = new LMS();
-            Akash.Display();
-            Akash.Message();
-            Akash.placement_record();
+            Console.WriteLine("Hello World!");
+            //implementing Records
 
+            //person CFO = new person();
+            //CFO.Fname = "mike";
+            //CFO.Lname = "scot";
 
-            IAssesment Delhi = Akash;
-            Delhi.MCQ_Assesment();
-            Delhi.QuestionBank();
+            Newperson CEO = new Newperson();
+            CEO.Firstname = "sathya";
+            CEO.LastName = "nalleda";
 
+            Coordinates India = new Coordinates(120, 220);
+            
+            Console.WriteLine("Indian coordinates are :" + India.X + India.Y);
+            
+
+            
 
         }
+    }
+    public record person(string Fname,string LName);
+    public record person1
+    {
+        public string First_Name { get; init; } =default!;
+        public string LastName { get; init; } = default!;
+    }
+    public record Newperson
+    {
+        public string Firstname { get; set; } = default!;
+        public string LastName { get; set; } = default!;
     }
 }
