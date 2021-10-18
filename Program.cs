9ebf63e6192a213d31ca42a5ab341e3180935ea3
vysh1234 <75359385+vysh1234@users.noreply.github.com>
2021-10-18 10:36:52 +0530

    Class implementation

--- Program.cs
+++ Program.cs
@@ -1,24 +1,22 @@
 ﻿using System;
-using System.IO;
 
-namespace file_handling
+namespace Demo_Class_Implementation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's see file handling:");
+            LMS Akash = new LMS();
+            Akash.Display();
+            Akash.Message();
+            Akash.placement_record();
 
-            FileStream fs = new FileStream("C:\\Users\\vaishnavi.vinjam\\source\\repos\\file_handling\\file.txt",FileMode.OpenOrCreate);
-            StreamReader sr = new StreamReader(fs);
-            Console.WriteLine("let's read and display from file");
 
-            string str = sr.ReadLine();
-            while(str!=null)
-            {
-                Console.WriteLine(str);
-                str = sr.ReadLine();
-            }
+            IAssesment Delhi = Akash;
+            Delhi.MCQ_Assesment();
+            Delhi.QuestionBank();
+
+
         }
     }
 }
